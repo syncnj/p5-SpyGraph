b22ff93184b8b25fb0aac4efd23dec6dd40e9903
David <davidguyuchen@gmail.com>
2016-04-30 16:12:15 -0500

    Update when shenyi cnm

---
+++ src/GraphNode.java
@@ -0,0 +1,11 @@
+/**
+ * Created by David on 4/30/16.
+ */
+public class GraphNode {
+    public static int NOT_NEIGHBOR;
+
+    public GraphNode(String name){
+
+    }
+
+}
--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,60 @@
 /**
  * Created by yblur on 4/28/2016.
  */
+
+
 public class Player {
+    /**
+name - Player's name
+budget - Player budget to spend on moves
+spycams - the number of spy cams the player starts with
+startnode - the node the player starts on
+ */
+
+   public Player(String name, int budget, int spycams, GraphNode startnode){
+
+
+   }
+
+    public void decreaseBudget(int dec){
+
+    }
+
+    public boolean dropSpycam(){
+        return false;
+    }
+    public int getBudget(){
+        return null;
+    }
+    public GraphNode getLocation(){
+
+    }
+    public String getLocationName(){
+        return null;
+    }
+    public String getName(){
+        return null;
+    }
+    public int getSpycamBack(boolean pickupSpyCam){
+        return -0;
+    }
+    public int getSpycams(){
+        return 0;
+    }
+    public boolean move(String name){
+        return false;
+    }
+    public boolean pickupSpycam(GraphNode node){
+        return false;
+    }
+    public void printSpyCamLocations(){
+
+    }
+
+
+
+
+
 
 }
 
